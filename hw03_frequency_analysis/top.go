c060e8b5455c6c507bdbfab8cbd1d2203b0ccd23
doncodes <shvetsov1988@gmail.com>
2022-04-01 19:07:40 +0200

    Top10 function

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -6,30 +6,29 @@ import (
 	"strings"
 )
 
-var regExp, _ = regexp.Compile("[^a-zA-Zа-яА-Я-]+")
-
-var delSyms = []string{"-"}
+var regExp = regexp.MustCompile("[^a-zA-Zа-яА-Я-]+")
 
 func Top10(text string) []string {
-	parsed := regExp.ReplaceAllString(text, " ")
-	parsed = strings.ToLower(parsed)
+	parsed := strings.ToLower(regExp.ReplaceAllString(text, " "))
 	words := strings.Fields(parsed)
 
-	var occurences = make(map[string]int)
+	occurrences := make(map[string]int)
 	for _, word := range words {
-		occurences[word] += 1
-	}
-	for _, delSym := range delSyms {
-		delete(occurences, delSym)
+		if word == "-" {
+			continue
+		}
+		occurrences[word]++
 	}
 
-	var unsortedWords []string
-	for word := range occurences {
-		unsortedWords = append(unsortedWords, word)
+	unsortedWords := make([]string, len(occurrences))
+	var idx int
+	for word := range occurrences {
+		unsortedWords[idx] = word
+		idx++
 	}
 
 	sort.Slice(unsortedWords, func(i, j int) bool {
-		if occurences[unsortedWords[i]] == occurences[unsortedWords[j]] {
+		if occurrences[unsortedWords[i]] == occurrences[unsortedWords[j]] {
 			switch strings.Compare(unsortedWords[i], unsortedWords[j]) {
 			case 1:
 				return false
@@ -38,11 +37,11 @@ func Top10(text string) []string {
 			}
 		}
 
-		return occurences[unsortedWords[i]] > occurences[unsortedWords[j]]
+		return occurrences[unsortedWords[i]] > occurrences[unsortedWords[j]]
 	})
 
 	if len(unsortedWords) < 10 {
-		return unsortedWords[:]
+		return unsortedWords
 	}
 
 	return unsortedWords[:10]
