9d96a2f35a21200a8cb9213b5113d33b90e33b1a
doncodes <shvetsov1988@gmail.com>
2022-04-13 23:48:43 +0200

    lru cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,44 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	_, exists := l.items[key]
+
+	if exists {
+		l.items[key].Value = cacheItem{key, value}
+		l.queue.MoveToFront(l.items[key])
+
+		return true
+	}
+
+	l.items[key] = l.queue.PushFront(cacheItem{key, value})
+	if l.capacity < l.queue.Len() {
+		delete(l.items, l.queue.Back().Value.(cacheItem).key)
+		l.queue.Remove(l.queue.Back())
+	}
+
+	return false
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	val, exists := l.items[key]
+
+	if exists {
+		l.queue.MoveToFront(val)
+		return val.Value.(cacheItem).value, true
+	}
+
+	return nil, false
+}
+
+func (l *lruCache) Clear() {
+	l.queue = NewList()
+	l.items = make(map[Key]*ListItem, l.capacity)
 }
 
 type cacheItem struct {
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -10,15 +10,130 @@ type List interface {
 	MoveToFront(i *ListItem)
 }
 
+type listItemState int
+
+const (
+	used listItemState = iota
+	removed
+)
+
 type ListItem struct {
 	Value interface{}
 	Next  *ListItem
 	Prev  *ListItem
+	state listItemState
 }
 
+type listState int
+
+const (
+	idle listState = iota
+	single
+	multiple
+)
+
+const (
+	incr string = "incr"
+	decr        = "decr"
+)
+
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	size  int
+	state listState
+	front *ListItem
+	back  *ListItem
+}
+
+func (l list) Len() int {
+	return l.size
+}
+
+func (l list) Front() *ListItem {
+	return l.front
+}
+
+func (l list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	oldFront := l.front
+	l.front = &ListItem{Value: v, Next: oldFront}
+
+	l.updState(incr)
+
+	if l.state == multiple {
+		oldFront.Prev = l.front
+	}
+
+	return l.front
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	oldBack := l.back
+	l.back = &ListItem{Value: v, Prev: oldBack}
+
+	l.updState(incr)
+
+	if l.state == multiple {
+		oldBack.Next = l.back
+	}
+
+	return l.back
+}
+
+func (l *list) updState(action string) {
+	switch action {
+	case incr:
+		if l.size++; l.size > 1 {
+			l.state = multiple
+		}
+	case decr:
+		if l.size--; l.size <= 1 {
+			l.state = single
+		}
+	}
+
+	if l.front == nil {
+		l.front = l.back
+	}
+
+	if l.back == nil {
+		l.back = l.front
+	}
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.state == removed {
+		return
+	}
+
+	l.updState(decr)
+
+	if l.front == i {
+		l.front = i.Next
+	}
+
+	if l.back == i {
+		l.back = i.Prev
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	i.Prev = nil
+	i.Next = nil
+	i.state = removed
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	l.PushFront(i.Value)
 }
 
 func NewList() List {
